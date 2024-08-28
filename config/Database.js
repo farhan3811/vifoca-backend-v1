6499d542dd5530aa4d141c99367f1fec835e780b
Farhan <muhammadfarhan@staff.unsia.ac.id>
2024-08-28 18:25:19 +0700

    devdepencies

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,10 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('vifocaa', 'postgres', 'wartam7317', {
+const db = new Sequelize(
+    'vifocaa',   // nama db
+    'postgres', // username
+    'wartam7317', // password
+    {
     host: "localhost",
     dialect: "postgres"
 });
