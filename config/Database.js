f7f81ef90b015b402e8f09acb12ef039ebba2dfc
Farhan <muhammadfarhan@staff.unsia.ac.id>
2024-08-28 15:38:30 +0700

    package dev

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,5 @@
 import { Sequelize } from "sequelize";
 
-// Membuat koneksi ke PostgreSQL
 const db = new Sequelize('vifocaa', 'postgres', 'wartam7317', {
     host: "localhost",
     dialect: "postgres"
